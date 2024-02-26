148ddb1c411d96d5fe09f20b2cdba78f697e6fb7
jgguida <jon.gui@aol.com>
2024-02-26 20:25:48 -0300

    Adicionando formulário simples ao GO

--- dia_2/main.go
+++ dia_2/main.go
@@ -2,14 +2,45 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/saudacao/", func(w http.ResponseWriter, r *http.Request) {
-		nome := r.URL.Path[len("/saudacao/"):]
-		fmt.Fprintf(w, "Olá, %s!", nome)
-	})
+	http.HandleFunc("/", formHandler)
+	http.HandleFunc("/saudacao", saudacaoHandler)
 
+	fmt.Println("Servidor rodando na porta 8080...")
 	http.ListenAndServe(":8080", nil)
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.New("form").Parse(`
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title>Formulário de Saudação</title>
+		</head>
+		<body>
+			<form action="/saudacao" method="post">
+				<label for="nome">Digite seu nome:</label>
+				<input type="text" id="nome" name="nome">
+				<input type="submit" value="Enviar">
+			</form>
+		</body>
+		</html>
+	`))
+	tmpl.Execute(w, nil)
+}
+
+func saudacaoHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Método não permitido", http.StatusMethodNotAllowed)
+		return
+	}
+
+	r.ParseForm()
+	nome := r.FormValue("nome")
+	fmt.Fprintf(w, "Olá, %s!", nome)                // Escreve na tela
+	w.Header().Set("X-Custom-Header", "Olá, "+nome) // Define cabeçalho personalizado
 }
